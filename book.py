36ff7aa9981bb795414e250dc750ab285ebed7a3
Lina Salameh <lina.salameh@edu.devinci.fr>
2021-02-25 19:52:19 +0100

    exercice 3

--- book.py
+++ book.py
@@ -1,13 +1,63 @@
 class Order:
-    def __init__(self, quantity, price, buy=True):
+    def __init__(self, quantity, price,id, buy=True):
         self.quantity=quantity
         self.price = price
         self.buy=buy
+        self.id = id
+
+
+
+    
 
 class Book:
     def __init__(self, name, buy_orders=[], sell_orders=[]):
         self.name=name
         self.buy_orders=buy_orders
         self.sell_orders=sell_orders
+        self.id=0
 
+    
+    def execute_orders(self):
+        for sell in self.sell_orders[:] :
+            for buy in self.buy_orders[:] :
+                if sell.price <= buy.price:
+                    if sell.quantity >= buy.quantity:
+                        print( "Execute " + str(buy.quantity) + " at " + str(buy.price) + " on " + self.name)
+                        sell.quantity = sell.quantity - buy.quantity
+                        buy.quantity = 0
+                    else:
+                        print( "Execute " + str(sell.quantity) + " at " + str(buy.price) + " on " + self.name)
+                        buy.quantity = buy.quantity - sell.quantity
+                        sell.quantity = 0
+                    if sell.quantity == 0:
+                        self.sell_orders.remove(sell)
+                        break;  
+                    if buy.quantity == 0:
+                        self.buy_orders.remove(buy)
+
+    def insert_buy(self,qty, price):
+        self.id =self.id + 1
+        self.buy_orders.append(Order(qty,price,self.id, True))
+        self.buy_orders.sort(key=lambda x: x.price, reverse= True)
+        print("--- Insert BUY " + str(qty) +"@"+ str(price) + " id= "+ str(self.id) +" on "+ self.name)
+        self.execute_orders()
+        self.print_book()
+
+    def insert_sell(self,qty, price):
+        self.id =self.id + 1  
+        self.sell_orders.append(Order(qty,price,self.id,False))
+        self.sell_orders.sort(key=lambda x: x.price, reverse= True)
+        print("--- Insert SELL " + str(qty) +"@"+ str(price) + " id= "+ str(self.id) + " on "+ self.name)
+        self.execute_orders()
+        self.print_book()
+        
+    def print_book(self):
+        print("Book on " + self.name)
+        for sell in self.sell_orders:
+            print(" SELL " + str(sell.quantity)  +"@"+ str(sell.price) + " id= "+ str(sell.id))
+        for buy in self.buy_orders :
+            print(" BUY " + str(buy.quantity)  +"@"+ str(buy.price) + " id= "+ str(buy.id))
+        print( "------------------------------------------------------" )
+    
+    
 
--- market.py
+++ market.py
@@ -1,15 +1,15 @@
  #!/usr/bin/env python3
 
- from book import Book
+from book import Book
 
- def main():
-     book = Book("TEST")
-     book.insert_buy(10, 10.0)
-     book.insert_sell(120, 12.0)
-     book.insert_buy(5, 10.0)
-     book.insert_buy(2, 11.0)
-     book.insert_sell(1, 10.0)
-     book.insert_sell(10, 10.0)
+def main():
+    book = Book("TEST")
+    book.insert_buy(10, 10.0)
+    book.insert_sell(120, 12.0)
+    book.insert_buy(5, 10.0)
+    book.insert_buy(2, 11.0)
+    book.insert_sell(1, 10.0)
+    book.insert_sell(10, 10.0)
 
- if __name__ == "__main__":
-     main()
+if __name__ == "__main__":
+    main()
